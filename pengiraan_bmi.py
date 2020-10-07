5cdcbe46da5242cd321994bbab7dc623b49eac7d
itsmeEVIL <amirshahrani05@gmail.com>
2020-10-08 03:03:05 +0800

    pengiraan_bmi: remove () from if statements

--- pengiraan_bmi.py
+++ pengiraan_bmi.py
@@ -7,6 +7,7 @@ print("***Atur Cara Untuk Mengira BMI Anda***\n")
 tinggi = input("Masukkan ketinggian anda (Meter): ")
 berat = input("Masukkan berat badan anda (KG): ")
 
+# Note - != bermaksud tidak sama dengan
 # Jika input tinggi dan berat tidak kosong
 if tinggi != "" and berat != "":
   
@@ -14,26 +15,26 @@ if tinggi != "" and berat != "":
 	# Tukar string kepada nombor
 	berat = float(berat)
 	tinggi = float(tinggi)
-	# Formula untuk mengira BMI - berat × tinggi^2
+	# Formula untuk mengira BMI - berat ÷ tinggi^2
 	bmi = berat / (tinggi * tinggi)
 
 	# Output
 	print("\nBMI Anda: ", round(bmi, 2), "\nDan Anda: ", end="")
 
 	# Jika bmi kurang daripada 18.5
-	if (bmi < 18.5):
+	if bmi < 18.5:
 		print("Kurang Berat Badan")
 
 	# Jika bmi lebih atau sama dengan 18.5 dan kurang daripada 24.9
-	elif (bmi >= 18.5 and bmi < 24.9):
+	elif bmi >= 18.5 and bmi < 24.9:
 		print("Berat Badan Normal")
 
 	# Jika bmi lebih atau sama dengan 25.0 dan kurang daripada 29.9
-	elif (bmi >= 25.0 and bmi < 29.9):
+	elif bmi >= 25.0 and bmi < 29.9:
 		print("Lebih Berat Badan")
 
 	# Jika bmi lebih atau sama dengan 30.0
-	elif (bmi >= 30.0):
+	elif bmi >= 30.0:
 		print("Obesiti")
 
 # Jika input tinggi tidak kosong dan berat kosong
@@ -46,4 +47,4 @@ elif tinggi == "" and berat != "":
 
 # Jika tinggi dan berat kosong
 else:
-	print("ERROR:\nMasukkan Ketinggian Dan Berat Badan Anda Dengan Betul!")
+  print("ERROR:\nMasukkan Ketinggian Dan Berat Badan Anda Dengan Betul!")
