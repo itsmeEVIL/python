0220b53f3fafbc1e8c39207f47d536967319ce20
itsmeEVIL <amirshahrani05@gmail.com>
2020-10-12 00:38:19 +0800

    pengiraan_bmi_gui: just figuring how tkinter works

--- ASK/pengiraan_bmi_gui.py
+++ ASK/pengiraan_bmi_gui.py
@@ -1,7 +1,37 @@
-# Import semua benda dari module Tkinter
-from tkinter import *
+import sys
+# Import module Tkinter
+if sys.version_info[0] == 3:
+    # Untuk Python3
+    from tkinter import * ## 't'kinter
+else:
+    # Untuk Python2
+    from Tkinter import * ## 'T'kinter
 
+# Window
 window = Tk()
+# Title window
+window.title("pengiraan_bmi")
+# Size window
+window.geometry("400x250")
 
+# Widget label
+label = Label(
+	window,
+	text="Hello!",
+	font=("Arial Bold", 40),
+	foreground="black",
+	background="blue"
+)
+# Widget Button
+button = Button(
+	text="this is clickable!",
+	foreground="black",
+	background="red"
+)
 
+# Meletakkan widget di dalam grid tertentu
+label.grid(row=0, column=0)
+button.grid(row=1, column=0)
+
+# Loop the window
 window.mainloop()
