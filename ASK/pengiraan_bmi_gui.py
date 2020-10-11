7ca3af63c700125b03f8c4aa84f2a9690c4a0521
itsmeEVIL <amirshahrani05@gmail.com>
2020-10-12 03:48:30 +0800

    pengiraan_bmi_gui: fixed the indentation

--- ASK/pengiraan_bmi_gui.py
+++ ASK/pengiraan_bmi_gui.py
@@ -10,86 +10,86 @@ else:
     
 # Functions
 def button_clicked():
-  tinggi = input_ketinggian.get()
-  berat = input_berat.get()
-  bmi = berat / (tinggi * tinggi)
-  bmi = str(bmi)
-  output_bmi.configure(text=bmi)
+	tinggi = input_ketinggian.get()
+    berat = input_berat.get()
+    bmi = berat / (tinggi * tinggi)
+    bmi = str(bmi)
+    output_bmi.configure(text=bmi)
 
 
 window = tk.Tk()
 
 canvas = tk.Canvas(
-  window,
-  bg='#0099ff',
-  width=600,
-  height=500
-  )
+    window,
+    bg='#0099ff',
+    width=600,
+    height=500
+    )
 canvas.pack()
 
 frame_input = tk.Frame(
-  window,
-  bg='#99d6ff',
-  bd=5
-  )
+    window,
+    bg='#99d6ff',
+    bd=5
+    )
 frame_input.place(
-  relx=0.5,
-  rely=0.1,
-  relwidth=0.75,
-  relheight=0.1,
-  anchor='n'
-  )
+    relx=0.5,
+    rely=0.1,
+    relwidth=0.75,
+    relheight=0.1,
+    anchor='n'
+    )
 
 input_ketinggian = tk.Entry(
-  frame_input,
-  font=40
-  )
+    frame_input,
+    font=40
+    )
 input_ketinggian.place(
-  relwidth=0.65,
-  relheight=1
-  )
+    relwidth=0.65,
+    relheight=1
+    )
 
 input_berat = tk.Entry(
-  frame_input,
-  font=40
-  )
+    frame_input,
+    font=40
+    )
 input_berat.place(
-  relx=0.15,
-  relwidth=0.65,
-  relheight=1
-  )
+    relx=0.15,
+    relwidth=0.65,
+    relheight=1
+    )
 
 button = tk.Button(
-  frame_input,
-  text="OK",
-  font=40,
-  command=button_clicked
-  )
+    frame_input,
+    text="OK",
+    font=40,
+    command=button_clicked
+    )
 button.place(
-  relx=0.7,
-  relheight=1,
-  relwidth=0.3
-  )
+    relx=0.7,
+    relheight=1,
+    relwidth=0.3
+    )
 
 frame_output = tk.Frame(
-  window,
-  bg='#99d6ff',
-  bd=5
-  )
+    window,
+    bg='#99d6ff',
+    bd=5
+    )
 frame_output.place(
-  relx=0.5,
-  rely=0.25,
-  relwidth=0.75,
-  relheight=0.6,
-  anchor='n'
-  )
+    relx=0.5,
+    rely=0.25,
+    relwidth=0.75,
+    relheight=0.6,
+    anchor='n'
+    )
 
 output_bmi = tk.Label(
-  frame_output
-  )
+    frame_output
+    )
 output_bmi.place(
-  relwidth=1,
-  relheight=1
-  )
+    relwidth=1,
+    relheight=1
+    )
 
 window.mainloop()
