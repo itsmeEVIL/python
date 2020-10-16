3e811ffeb733ad72b6f37ee63340da2b4f88b92a
itsmeEVIL <amirshahrani05@gmail.com>
2020-10-16 16:02:38 +0800

    calories: fixed some typos

--- calories.py
+++ calories.py
@@ -5,39 +5,39 @@ print('***RECOMMEND THE NUMBER OF CALORIES ALLOWED BASED ON GENDER AND AGE***')
 
 # input
 # gender (yes = male, no = female)
-gender = input('Enter Your Gender (MALE/FEMALE: ')
+gender = input('Enter Your Gender (MALE/FEMALE): ')
 
 # proses
 # if gender is male
 if gender.lower() == 'male':
 	# input
 	# age
-	age = int(input('What Is Your Age?'))
+	age = int(input('What Is Your Age? '))
 
 	# if age more or equal to 60
 	if (age >= 60):
 		# output
-		print('The allowable caloric value is 2010')
+		print('The allowed caloric value is 2010')
 
 	# if age more or equal to 30
 	elif (age >= 30):
 		# output
-		print('The allowable caloric value is 2460')
+		print('The allowed caloric value is 2460')
 
 	# if age more or equal to 19
 	elif (age >= 19):
 		# output
-		print('The allowable caloric value is 2440')
+		print('The allowed caloric value is 2440')
 
 	# if age more or equal to 16
 	elif (age >= 16):
 		# output
-		print('The allowable caloric value is 2840')
+		print('The allowed caloric value is 2840')
 
 	# if age more or equal to 13
 	elif (age >= 13):
 		# output
-		print('The allowable caloric value is 2690')
+		print('The allowed caloric value is 2690')
 
 	# if age is less than 13
 	else:
@@ -48,32 +48,32 @@ if gender.lower() == 'male':
 elif gender.lower() == 'female':
 	# input
 	# umur
-	age = int(input('What Is Your Age?'))
+	age = int(input('What Is Your Age? '))
 
 	# if age more or equal to 60
 	if (age >= 60):
 		# output
-		print('The allowable caloric value is 1780')
+		print('The allowed caloric value is 1780')
 
 	# if age more or equal to 30
 	elif (age >= 30):
 		# output
-		print('The allowable caloric value is 2180')
+		print('The allowed caloric value is 2180')
 
 	# if age more or equal to 19
 	elif (age >= 19):
 		# output
-		print('The allowable caloric value is 2000')
+		print('The allowed caloric value is 2000')
 
 	# if age more or equal to 16
 	elif (age >= 16):
 		# output
-		print('The allowable caloric value is 2050')
+		print('The allowed caloric value is 2050')
 
 	# if age more or equal to 13
 	elif (age >= 13):
 		# output
-		print('The allowable caloric value is 2180')
+		print('The allowed caloric value is 2180')
 
 	# if age less than 13
 	else:
@@ -83,4 +83,4 @@ elif gender.lower() == 'female':
 # if there's no input or wrong input
 else:
     # output
-	print('Thank You')
+	print('ERROR:\nPlease Enter Your Gender Correctly!')
