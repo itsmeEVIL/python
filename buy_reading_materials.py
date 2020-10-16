c5e03d67daace68250ba4586b9ccbc50b786b673
itsmeEVIL <amirshahrani05@gmail.com>
2020-10-16 16:23:02 +0800

    buy_reading_materials: translated to english

--- buy_reading_materials.py
+++ buy_reading_materials.py
@@ -1,39 +1,39 @@
-# Atur cara untuk mengira harga buku
+# program to calculate the price of the book
 
-# Pemboleh ubah
-# Harga-harga buku
-# Harga buku cerita
-buku_cerita = float(39)
-# Harga majalah
-majalah = float(16)
+# variables
+# price of books
+# price of story book
+story_book = float(39)
+# price of magazine
+magazine = float(16)
 
-# Proses
-# Jumlah buku cerita dan majalah
-jum1 = (buku_cerita * 2) + majalah
-# Bayaran yang dibuat
-bayaran = 200
-# Baki yang tinggal
-baki1 = 66
+# proses
+# total cost of story book and magazine
+total1 = (story_book * 2) + magazine
+# payment made
+payment = 200
+# remainder
+remainder1 = 66
 
-# Jumlah harga semua buku
-jumlah_buku = bayaran - baki1
-# Harga senaskah buku motivasi
-buku_motisasi = (jumlah_buku - jum1) / 2
+# total cost of all book
+total_book_cost = payment - remainder1
+# cost of 1 motivational book
+motivational_book = (total_book_cost - total1) / 2
 
-# Semakan Pengiraan
-# Jumlah harga semua buku
-jum3 = jum1 + (2 * buku_motisasi)
-# Baki
-baki2 = 200 - jum3
+# calculation review
+# total cost of all book
+total2 = total1 + (2 * motivational_book)
+# remainder
+remainder2 = 200 - total2
 
-# Output
-# Bayaran yang dibuat
-print('Bayaran yang dibuat: RM200')
-# Harga 2 buku cerita
-print('\nBuku cerita: RM', 2 * buku_cerita)
-# Harga majalah
-print('Majalah: RM', majalah)
-# Harga senaskah buku motivasi
-print('Buku motivasi: RM', buku_motisasi)
-# Baki wang selepas pembayaran
-print('\nBaki wang anda: RM', baki2, '\nSemakan adalah:', baki2==baki1) #True bermaksud semakan benar
+# output
+# payment made
+print('Payment Made: RM200')
+# cost of 2 story book
+print('\nStory book: RM', 2 * story_book)
+# cost of a magazine
+print('Magazine: RM', magazine)
+# cost of a motivational book
+print('Motivational book: RM', motivational_book)
+# remainder after payment was made
+print('\nYour remainder: RM', remainder2, '\nCalculation review:', remainder2 == remainder1)
