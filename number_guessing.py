c2b4a4d2d00471f9d56ecf63b1340a22643a6861
itsmeEVIL <evilsgonnacry@gmail.com>
2020-10-28 02:20:08 +0800

    number_guessing: edited
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- number_guessing.py
+++ number_guessing.py
@@ -6,21 +6,21 @@ from random import randint
 
 print("***Number Guessing Game***\nYou only got 5 chances to guess the correct number")
 
-rand_num = randint(1, 10) # make random integer between 1 and 10
-u_inp = int(input("\nChoose a number between 1 and 10: "))
+random_number = randint(1, 10) # make random integer between 1 and 10
+user_input = int(input("\nChoose a number between 1 and 10: "))
 chance = 5 
 
 while chance != 0:
 	chance -= 1 # minus chance until answer is correct
-	if u_inp != rand_num:
-		if u_inp <= rand_num:
+	if user_input != random_number:
+		if user_input <= random_number:
 			print("\nNumber entered is smaller than the generated number!\nYou got", chance, "chance(s) left!")
-			u_inp = int(input("Enter bigger number: "))
+			user_input = int(input("Enter a bigger number: "))
 		else:
 			print("\nNumber entered is bigger than the number generated!\nYou got", chance, "chance(s) left!")
-			u_inp = int(input("Enter smaller number: "))
+			user_input = int(input("Enter a smaller number: "))
 	else:
-		print("\nThe number you've guessed, ", u_inp, ", is correct.")
+		print("\nThe number you've guessed, ", user_input, ", is correct.")
 		break # break out from the while loop
 else:
 	print("\nYou got no chance left!\nBetter luck next time!")
