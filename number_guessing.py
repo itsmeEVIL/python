55c58a612ce8d779b09e725c24f340d26b70b491
itsmeEVIL <evilsgonnacry@gmail.com>
2020-10-28 19:52:30 +0800

    number_guessing: break if chance reach 0
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- number_guessing.py
+++ number_guessing.py
@@ -10,17 +10,22 @@ random_number = randint(1, 10) # make random integer between 1 and 10
 user_input = int(input("\nChoose a number between 1 and 10: "))
 chance = 5 
 
-while chance != 0:
+while chance <= 5:
 	chance -= 1 # minus chance until answer is correct
-	if user_input != random_number:
-		if user_input <= random_number:
-			print("\nNumber entered is smaller than the generated number!\nYou got", chance, "chance(s) left!")
-			user_input = int(input("Enter a bigger number: "))
-		else:
-			print("\nNumber entered is bigger than the number generated!\nYou got", chance, "chance(s) left!")
-			user_input = int(input("Enter a smaller number: "))
+	if chance == 0:
+		print("\nYou got no chance left!\nBetter luck next time!")
+		break
 	else:
-		print("\nThe number you've guessed, ", user_input, ", is correct.")
-		break # break out from the while loop
+		if user_input != random_number:
+			if user_input <= random_number:
+				print("\nNumber entered is smaller than the generated number!\nYou got", chance, "chance(s) left!")
+				user_input = int(input("Enter a bigger number: "))
+			else:
+				print("\nNumber entered is bigger than the number generated!\nYou got", chance, "chance(s) left!")
+				user_input = int(input("Enter a smaller number: "))
+		else:
+			print("\nThe number you've guessed, ", user_input, ", is correct.")
+			break # break out from the while loop
+		
 else:
 	print("\nYou got no chance left!\nBetter luck next time!")
