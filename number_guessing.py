bcd0675c5ca5f8fd7589dcd8eb1c88a1c4abcc21
itsmeEVIL <evilsgonnacry@gmail.com>
2020-10-28 19:59:48 +0800

    number_guessing: edited
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- number_guessing.py
+++ number_guessing.py
@@ -1,12 +1,12 @@
 # program about number guessing
 # made by itsmeevil
 
-# import random integer from random module
-from random import randint
+# import random module
+import random as rd
 
 print("***Number Guessing Game***\nYou only got 5 chances to guess the correct number")
 
-random_number = randint(1, 10) # make random integer between 1 and 10
+random_number = rd.randint(1, 10) # make random integer number  between 1 and 10
 user_input = int(input("\nChoose a number between 1 and 10: "))
 chance = 5 
 
@@ -24,8 +24,6 @@ while chance <= 5:
 				print("\nNumber entered is bigger than the number generated!\nYou got", chance, "chance(s) left!")
 				user_input = int(input("Enter a smaller number: "))
 		else:
-			print("\nThe number you've guessed, ", user_input, ", is correct.")
+			print("\nGreat job!\nThe number you've guessed, ", user_input, ", is correct.")
 			break # break out from the while loop
-		
-else:
-	print("\nYou got no chance left!\nBetter luck next time!")
+
