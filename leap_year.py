c3b9c21eb135791f8f17ec7e6f84bd38c010e0cc
itsmeEVIL <evilsgonnacry@gmail.com>
2020-11-02 23:15:42 +0800

    leap_year: fixed
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- leap_year.py
+++ leap_year.py
@@ -12,11 +12,12 @@ print("Checking...")
 time.sleep(2) # using the function sleep from time module to sleep for 2 seconds
 
 if (year % 4) == 0:
-	print("\nThe year entered is a leap year.")
 	if (year % 100) == 0:
-		print("\nThe year entered is not a leap year.")
-	else:
 		if (year % 400) == 0:
-			print("\nThe year entered is a leap year.")
+			print("\nThe year", year, "is a leap year.")
+		else:
+			print("\nThe year", year, "is not a leap year.")
+	else:
+		print("\nThe year", year, "is a leap year.")
 else:
-	print("\nThe year entered is not a leap year.")
+	print("\nThe year", year, "is not a leap year.")
