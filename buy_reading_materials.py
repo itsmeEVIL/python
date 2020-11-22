629a75373027c0715cc118566187c7871ca9b2d2
itsmeEVIL <evilsgonnacry@gmail.com>
2020-11-22 22:33:56 +0800

    buy_reading_material: use f-strings

--- buy_reading_materials.py
+++ buy_reading_materials.py
@@ -1,40 +1,21 @@
 # program to calculate the price of the book
 # made by itsmeevil
 
-# variables
-# price of books
-# price of story book
 story_book = float(39)
-# price of magazine
 magazine = float(16)
 
-# proses
-# total cost of story book and magazine
 total1 = (story_book * 2) + magazine
-# payment made
 payment = 200
-# remainder
 remainder1 = 66
 
-# total cost of all book
 total_book_cost = payment - remainder1
-# cost of 1 motivational book
 motivational_book = (total_book_cost - total1) / 2
 
-# calculation review
-# total cost of all book
 total2 = total1 + (2 * motivational_book)
-# remainder
 remainder2 = 200 - total2
 
-# output
-# payment made
-print('Payment Made: RM200')
-# cost of 2 story book
-print('\nStory book: RM', 2 * story_book)
-# cost of a magazine
-print('Magazine: RM', magazine)
-# cost of a motivational book
-print('Motivational book: RM', motivational_book)
-# remainder after payment was made
-print('\nYour remainder: RM', remainder2, '\nCalculation review:', remainder2 == remainder1)
+print("Payment Made: RM200")
+print(f"\nStory book: RM {2 * story_book}")
+print(f"Magazine: RM {magazine}")
+print(f"Motivational book: RM {motivational_book}")
+print(f"\nYour remainder: RM {remainder2}\nCalculation review: {remainder2 == remainder1}")
