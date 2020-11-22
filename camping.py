9128dce54a668446ac56fb7754762b1736e15b08
itsmeEVIL <evilsgonnacry@gmail.com>
2020-11-22 23:08:05 +0800

    camping: use f-strings
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- camping.py
+++ camping.py
@@ -1,28 +1,15 @@
 # program to calculate the total cost of camping in uniformed units  
 # made by itsmeevil
 
-# variables
-# cost of items in fixed cost
-# cost of personal equipment
 personal = float(13.50)
-# cost of clothing
 clothing = float(105.90)
-# tent rental costs
 tent = float(12.00)
 
-# process
-# fixed cost
 fixed_cost = personal + clothing + tent
-# cooking costs
-cooking_cost = float(input('\nEnter the cost for cooking: RM'))
-# total cost
+cooking_cost = float(input("\nEnter the cost for cooking: RM "))
 total_cost = fixed_cost + cooking_cost
 
-# output
-print('\n***Calculation of Uniformed Unit Camping Cost***')
-# total fixed cost
-print('\nTotal of Fixed Cost: RM', fixed_cost)
-# total costs change
-print('Total of Changed Cost: RM', cooking_cost)
-# total of all costs
-print('Total of All Costs: RM', round(total_cost, 2))
+print("\n***Calculation of Uniformed Unit Camping Cost***")
+print(f"\nTotal of Fixed Cost: RM {fixed_cost}")
+print(f"Total of Changed Cost: RM {cooking_cost}")
+print(f"Total of All Costs: RM {round(total_cost, 2)}")
