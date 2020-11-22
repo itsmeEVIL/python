335f849400dfa64837e7c517ae4aa364ba5d4b42
itsmeEVIL <evilsgonnacry@gmail.com>
2020-11-22 23:12:27 +0800

    dice: use f-strings
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- dice.py
+++ dice.py
@@ -1,21 +1,16 @@
 # program about rolling dice
 # made by itsmeevil
 
-# import random module
-import random as rd
-
-def roll():
-	rolled = str(rd.randint(1, 6)) # generate random integer number between 1 and 6
-	rolled = "Dice rolled. You got " + rolled
-	return rolled # return rolled variable when function is called
+import random
 
 print("***Roll The Dice***")
 ans = input("\nRoll dice? (Y/N): ").lower() # lowercase the input
 
 while ans == "y":
 	if ans == "y":
-		print(roll())
-		ans = input("\nRoll dice again? (Y/N): ")
+		rolled = str(random.randint(1, 6)) # generate random integer number between 1 and 6
+		print(f"Dice rolled. You got {rolled}")
+		ans = input("\nRoll dice again? (Y/N): ").lower()
 		if ans == "n":
 			print("Thanks for using!")
 			break # break from the loop
