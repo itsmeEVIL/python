0ac294a54bc41cdd7bd2cfbd826a37e202fa1d64
itsmeEVIL <evilsgonnacry@gmail.com>
2021-01-08 00:16:25 +0800

    calories: use spaces istead of tabs
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- calories.py
+++ calories.py
@@ -7,30 +7,30 @@ gender = input("\nEnter Your Gender (MALE/FEMALE): ").lower()
 age = input("Enter your age: ")
 
 if gender == "male":
-	if age >= "60":
-		print("\nThe allowed caloric value is 2010")
-	elif age >= "30":
-		print("\nThe allowed caloric value is 2460")
-	elif age >= "19":
-		print("\nThe allowed caloric value is 2440")
-	elif age >= "16":
-		print("\nThe allowed caloric value is 2840")
-	elif age >= "13":
-		print("\nThe allowed caloric value is 2690")
-	else:
-		print("\nSorry, age limit must be 13 years and above.")
+    if age >= "60":
+        print("\nThe allowed caloric value is 2010")
+    elif age >= "30":
+        print("\nThe allowed caloric value is 2460")
+    elif age >= "19":
+        print("\nThe allowed caloric value is 2440")
+    elif age >= "16":
+        print("\nThe allowed caloric value is 2840")
+    elif age >= "13":
+        print("\nThe allowed caloric value is 2690")
+    else:
+        print("\nSorry, age limit must be 13 years and above.")
 elif gender == "female":
-	if age >= "60":
-		print("\nThe allowed caloric value is 1780")
-	elif age >= "30":
-		print("\nThe allowed caloric value is 2180")
-	elif age >= "19":
-		print("\nThe allowed caloric value is 2000")
-	elif age >= "16":
-		print("\nThe allowed caloric value is 2050")
-	elif age >= "13":
-		print("\nThe allowed caloric value is 2180")
-	else:
-		print("\nSorry, age limit must be 13 years and above.")
+    if age >= "60":
+        print("\nThe allowed caloric value is 1780")
+    elif age >= "30":
+        print("\nThe allowed caloric value is 2180")
+    elif age >= "19":
+        print("\nThe allowed caloric value is 2000")
+    elif age >= "16":
+        print("\nThe allowed caloric value is 2050")
+    elif age >= "13":
+        print("\nThe allowed caloric value is 2180")
+    else:
+        print("\nSorry, age limit must be 13 years and above.")
 else:
-	print("\nERROR:\nPlease Enter Your Gender Correctly!")
+    print("\nERROR:\nPlease Enter Your Gender Correctly!")
