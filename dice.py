aa62d11613d412b376d5e6433dbb6a6738b70f67
itsmeEVIL <evilsgonnacry@gmail.com>
2021-01-08 00:16:45 +0800

    dice: use spaces istead of tabs
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- dice.py
+++ dice.py
@@ -4,15 +4,16 @@
 import random
 
 print("***Roll The Dice***")
-ans = input("\nRoll dice? (Y/N): ").lower() # lowercase the input
+ans = input("\nRoll dice? (Y/N): ").lower()  # lowercase the input
 
 while ans == "y":
-	if ans == "y":
-		rolled = str(random.randint(1, 6)) # generate random integer number between 1 and 6
-		print(f"Dice rolled. You got {rolled}")
-		ans = input("\nRoll dice again? (Y/N): ").lower()
-		if ans == "n":
-			print("Thanks for using!")
-			break # break from the loop
+    if ans == "y":
+        # generate random integer number between 1 and 6
+        rolled = str(random.randint(1, 6))
+        print(f"Dice rolled. You got {rolled}")
+        ans = input("\nRoll dice again? (Y/N): ").lower()
+        if ans == "n":
+            print("Thanks for using!")
+            break  # break from the loop
 else:
-	print(":/")
+    print(":/")
