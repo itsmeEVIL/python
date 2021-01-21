43391a5685eef268c5962a0470ce34ab60ff6fad
itsmeEVIL <evilsgonnacry@gmail.com>
2021-01-21 23:42:46 +0800

    average.py: renamed variable & deleted unnecessary comments
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- average.py
+++ average.py
@@ -1,16 +1,14 @@
 # program to calculate the average from a list of numbers
 # made by itsmeevil
 
-def average(num_list):
-    # sum() to sum up all the numbers
-    # len() to calc the length of a list
-    return sum(num_list) / len(num_list)
+def average(numList):
+    return round(sum(numList) / len(numList), 2)
 
 print("***Average Calculator***\n")
 
-num_list = input("Enter a list of numbers: ")
-num_list = num_list.split(" ") # put the nums into a list
-num_list = list(map(float, num_list)) # map() to change the content of the list from str to float
-average = round(average(num_list), 2) # call the function and round the returned value
+numList = input("Enter a list of numbers: ")
+numList = numList.split(" ")
+numList = list(map(float, numList))
+average = average(numList)
 
 print(f"\nThe average for the list of numbers entered: {average}")
