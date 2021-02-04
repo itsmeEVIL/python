9d4bc396acf2fb3112f7d2b36b74495d2513e9c0
itsmeEVIL <evilsgonnacry@gmail.com>
2021-02-04 20:13:31 +0800

    average.py: moved into the function make sense to put it inside the function ¯\_(0_0)_/¯
    Signed-off-by: itsmeEVIL <evilsgonnacry@gmail.com>

--- average.py
+++ average.py
@@ -2,13 +2,13 @@
 # made by itsmeevil
 
 def average(numList):
+    numList = numList.split(" ")
+    numList = list(map(float, numList))
     return round(sum(numList) / len(numList), 2)
 
 print("***Average Calculator***\n")
 
 numList = input("Enter a list of numbers: ")
-numList = numList.split(" ")
-numList = list(map(float, numList))
 average = average(numList)
 
 print(f"\nThe average for the list of numbers entered: {average}")
